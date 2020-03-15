e78a1a33511a9112387759b32828e7061373d1f7
odinlake <mikael@odinlake.net>
2020-03-15 14:28:29 +0000

    initial commit & express.js basic setup

---
+++ script/run.bat
@@ -0,0 +1,14 @@
+@echo off
+
+setlocal
+set DIR=%~dp0
+set PATH=%PATH%;%DIR%
+set PYTHONPATH=%PYTHONPATH%;%DIR%\..\pylib
+
+IF "%1"=="" (
+    start cmd
+) ELSE (
+    REM needed for some reason
+    %*
+)
+
